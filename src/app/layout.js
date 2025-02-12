ef927a9f7c4fdfc2270c05d396bc708179a44c23
Yamisa <majijayamisa@gmail.com>
2025-02-12 10:44:59 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+            <head>
+            <script
+          src="https://assets.adobedtm.com/43e08a8c9425/001e5275eba1/launch-5c5def145b42-development.min.js"
+          async
+        ></script>
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   )
