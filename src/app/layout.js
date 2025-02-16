786c57d97f4f46afd076377681a0cf2cfde77c17
Yamisa <111047781+Yamisa11@users.noreply.github.com>
2025-02-16 19:28:01 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
             <head>
-            <script
-          src="https://assets.adobedtm.com/43e08a8c9425/001e5275eba1/launch-5c5def145b42-development.min.js"
-          async
-        ></script>
+        //     <script
+        //   src="https://assets.adobedtm.com/43e08a8c9425/001e5275eba1/launch-5c5def145b42-development.min.js"
+        //   async
+        // ></script>
       </head>
 
       <body className={inter.className}>{children}</body>
